46d922596babeb875ec1782341f1e339078e88dd
SonuChopra <53923433+SonuChopra@users.noreply.github.com>
2020-04-05 23:48:01 -0400

    Added shaking of head

--- src1.js
+++ src1.js
@@ -1,67 +1,71 @@
 let COUNTRY = "US";
 
-function setDate(param){
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function setDate(param) {
   let lastUpdatedDate = new Date(param);
   document.getElementById("h3-top-center-div-label").innerHTML = "As of " + lastUpdatedDate.toDateString();
 }
 
-function setDaysLeft(param){
+function setDaysLeft(param) {
   document.getElementById("h1-body-days-displayed").innerHTML = param;
   document.getElementById("h1-display-days-left").innerHTML = param;
 }
 
-function setConfirmed(param1, param2){
-  document.getElementById("h3-bottom-section-label-infected").innerHTML = "Confirmed " + param1 + " (+" + param2 + ")";
+function setConfirmed(param1, param2) {
+  document.getElementById("h3-bottom-section-label-infected").innerHTML = "Confirmed " + numberWithCommas(param1) + " (+" + numberWithCommas(param2) + ")";
 }
 
-function setRecovered(param1, param2){
-  document.getElementById("h3-bottom-section-label-recovered").innerHTML = "Recovered " + param1 + " (+" + param2 + ")";
+function setRecovered(param1, param2) {
+  document.getElementById("h3-bottom-section-label-recovered").innerHTML = "Recovered " + numberWithCommas(param1) + " (+" + numberWithCommas(param2) + ")";
 }
 
-function setDeaths(param1, param2){
-  document.getElementById("h3-bottom-section-label-deaths").innerHTML = "Deaths " + param1 + " (+" + param2 + ")";
+function setDeaths(param1, param2) {
+  document.getElementById("h3-bottom-section-label-deaths").innerHTML = "Deaths " + numberWithCommas(param1) + " (+" + numberWithCommas(param2) + ")";
 }
 
-function getDaysLeft(){
+function getDaysLeft() {
   let today = new Date();
-  if(today.getMonth() === 3) {
+  if (today.getMonth() === 3) {
     return 45 - (today.getDate() - 3);
-  } else if(today.getMonth() === 4){
+  } else if (today.getMonth() === 4) {
     return 45 - (today.getDate() - 30);
   }
 }
 
-function getTodaysData(data){
+function getTodaysData(data) {
   return data[COUNTRY][data[COUNTRY].length - 1];
 }
 
-function getTodaysNewConfirmed(data){
+function getTodaysNewConfirmed(data) {
   return data[COUNTRY][data[COUNTRY].length - 1].confirmed - data[COUNTRY][data[COUNTRY].length - 2].confirmed;
 }
 
-function getTodaysNewRecovered(data){
+function getTodaysNewRecovered(data) {
   return data[COUNTRY][data[COUNTRY].length - 1].recovered - data[COUNTRY][data[COUNTRY].length - 2].recovered;
 }
 
-function getTodaysNewDeaths(data){
+function getTodaysNewDeaths(data) {
   return data[COUNTRY][data[COUNTRY].length - 1].deaths - data[COUNTRY][data[COUNTRY].length - 2].deaths;
 }
 
-function getTodaysRateConfirmed(data){
+function getTodaysRateConfirmed(data) {
   return (((data[COUNTRY][data[COUNTRY].length - 1].newConfirmed - data[COUNTRY][data[COUNTRY].length - 2].newConfirmed) / data[COUNTRY][data[COUNTRY].length - 2].newConfirmed) * 100)
 }
 
-function getTodaysRateRecovered(data){
+function getTodaysRateRecovered(data) {
   return ((data[COUNTRY][data[COUNTRY].length - 1].newRecovered - data[COUNTRY][data[COUNTRY].length - 2].newRecovered) / data[COUNTRY][data[COUNTRY].length - 2].newRecovered) * 100
 }
 
-function getTodaysRateDeaths(data){
+function getTodaysRateDeaths(data) {
   return ((data[COUNTRY][data[COUNTRY].length - 1].newDeaths - data[COUNTRY][data[COUNTRY].length - 2].newDeaths) / data[COUNTRY][data[COUNTRY].length - 2].newDeaths) * 100
 }
 
-function setTodaysConfirmedRate(param1){
+function setTodaysConfirmedRate(param1) {
   document.getElementById("h4-img-top-section-text-confirmed").innerHTML = Math.abs(parseInt(param1, 10)) + "%";
-  if (param1 >= 0){
+  if (param1 >= 0) {
     document.getElementById("h4-img-top-section-text-confirmed").style.color = "rgb(250, 109, 98)"
     document.getElementById("img-top-section-arrows-confirmed").src = "uparrow.png";
   } else {
@@ -70,9 +74,9 @@ function setTodaysConfirmedRate(param1){
   }
 }
 
-function setTodaysCoveredRate(param1){
+function setTodaysCoveredRate(param1) {
   document.getElementById("h4-img-top-section-text-recovered").innerHTML = Math.abs(parseInt(param1, 10)) + "%";
-  if (param1 >= 0){
+  if (param1 >= 0) {
     document.getElementById("h4-img-top-section-text-recovered").style.color = "rgb(50,205,50)"
     document.getElementById("img-top-section-arrows-recovered").src = "uparrow.png";
   } else {
@@ -81,9 +85,9 @@ function setTodaysCoveredRate(param1){
   }
 }
 
-function setTodaysDeathsRate(param1){
+function setTodaysDeathsRate(param1) {
   document.getElementById("h4-img-top-section-text-deaths").innerHTML = Math.abs(parseInt(param1, 10)) + "%";
-  if (param1 >= 0){
+  if (param1 >= 0) {
     document.getElementById("h4-img-top-section-text-deaths").style.color = "rgb(250, 109, 98)"
     document.getElementById("img-top-section-arrows-deaths").src = "uparrow.png";
   } else {
@@ -92,104 +96,119 @@ function setTodaysDeathsRate(param1){
   }
 }
 
-function getYBasedOnX(perm){
-  return ((2.165)*perm);
+function getYBasedOnX(perm) {
+  return ((2.165) * perm);
 }
 
-function pushMenitaCloser(){
+function pushMenitaCloser() {
   document.getElementById("img-body-menita").style.left = (document.getElementById("img-body-menita").offsetLeft + 5) + "px";
   let newY = getYBasedOnX(document.getElementById("img-body-menita").offsetLeft);
   document.getElementById("img-body-menita").style.top = parseInt(newY, 10) + "px";
 }
 
-function placeMenita(perm){
+function placeMenita(perm) {
   let today = new Date();
   let todaysDate;
-  if(today.getMonth() === 3) {
+  if (today.getMonth() === 3) {
     todaysDate = today.getDate() - 3;
-  } else if(today.getMonth() === 4) {
+  } else if (today.getMonth() === 4) {
     todaysDate = today.getDate() + 27
   }
-  for (let i=0; i<todaysDate; i++){
+  for (let i = 0; i < todaysDate; i++) {
     pushMenitaCloser();
   }
 }
 
-function setNewForTodayAndYesturdayData(data){
-    data[COUNTRY][data[COUNTRY].length - 1].newConfirmed = data[COUNTRY][data[COUNTRY].length - 1].confirmed - data[COUNTRY][data[COUNTRY].length - 2].confirmed
-    data[COUNTRY][data[COUNTRY].length - 1].newRecovered = data[COUNTRY][data[COUNTRY].length - 1].recovered - data[COUNTRY][data[COUNTRY].length - 2].recovered
-    data[COUNTRY][data[COUNTRY].length - 1].newDeaths = data[COUNTRY][data[COUNTRY].length - 1].deaths - data[COUNTRY][data[COUNTRY].length - 2].deaths
+function setNewForTodayAndYesturdayData(data) {
+  data[COUNTRY][data[COUNTRY].length - 1].newConfirmed = data[COUNTRY][data[COUNTRY].length - 1].confirmed - data[COUNTRY][data[COUNTRY].length - 2].confirmed
+  data[COUNTRY][data[COUNTRY].length - 1].newRecovered = data[COUNTRY][data[COUNTRY].length - 1].recovered - data[COUNTRY][data[COUNTRY].length - 2].recovered
+  data[COUNTRY][data[COUNTRY].length - 1].newDeaths = data[COUNTRY][data[COUNTRY].length - 1].deaths - data[COUNTRY][data[COUNTRY].length - 2].deaths
 
-    data[COUNTRY][data[COUNTRY].length - 2].newConfirmed = data[COUNTRY][data[COUNTRY].length - 2].confirmed - data[COUNTRY][data[COUNTRY].length - 3].confirmed
-    data[COUNTRY][data[COUNTRY].length - 2].newRecovered = data[COUNTRY][data[COUNTRY].length - 2].recovered - data[COUNTRY][data[COUNTRY].length - 3].recovered
-    data[COUNTRY][data[COUNTRY].length - 2].newDeaths = data[COUNTRY][data[COUNTRY].length - 2].deaths - data[COUNTRY][data[COUNTRY].length - 3].deaths
+  data[COUNTRY][data[COUNTRY].length - 2].newConfirmed = data[COUNTRY][data[COUNTRY].length - 2].confirmed - data[COUNTRY][data[COUNTRY].length - 3].confirmed
+  data[COUNTRY][data[COUNTRY].length - 2].newRecovered = data[COUNTRY][data[COUNTRY].length - 2].recovered - data[COUNTRY][data[COUNTRY].length - 3].recovered
+  data[COUNTRY][data[COUNTRY].length - 2].newDeaths = data[COUNTRY][data[COUNTRY].length - 2].deaths - data[COUNTRY][data[COUNTRY].length - 3].deaths
 }
 
-function setPicture(confirmRate, recoverRate, deathRate){
+function setPicture(confirmRate, recoverRate, deathRate) {
   let goodPoints = 0;
-  if(confirmRate < 0) {
+  if (confirmRate < 0) {
     goodPoints = goodPoints + 1;
-  } 
+  }
   if (recoverRate > 0) {
     goodPoints = goodPoints + 1;
   }
-  if(deathRate < 0){
+  if (deathRate < 0) {
     goodPoints = goodPoints + 1;
   }
-  if(goodPoints === 3) {
+  if (goodPoints === 3) {
     document.getElementById("img-body-face").src = "5.png";
   }
-  if(goodPoints === 0) {
+  if (goodPoints === 0) {
     document.getElementById("img-body-face").style.width = "340px";
     document.getElementById("img-body-face").style.height = "auto";
     document.getElementById("img-body-face").style.left = "74%";
     document.getElementById("img-body-face").src = "1.png";
   }
-  if(goodPoints === 1){
+  if (goodPoints === 1) {
     document.getElementById("img-body-face").src = "4.png";
   }
-  if(goodPoints === 2){
+  if (goodPoints === 2) {
     document.getElementById("img-body-face").src = "7.png";
   }
-  if(recoverRate > 20 && goodPoints == 2) {
+  if (recoverRate > 20 && goodPoints == 2) {
     document.getElementById("img-body-face").style.width = "380px";
     document.getElementById("img-body-face").style.height = "auto";
     document.getElementById("img-body-face").style.left = "74%";
     document.getElementById("img-body-face").src = "8.png";
   }
-  if(confirmRate > 10 && goodPoints == 1) {
+  if (confirmRate > 10 && goodPoints == 1) {
     document.getElementById("img-body-face").src = "2.png";
   }
-  if(confirmRate < 10 && goodPoints == 1) {
+  if (confirmRate < 10 && goodPoints == 1) {
     document.getElementById("img-body-face").src = "6.png";
   }
 }
 
+let shaked = true;
+
+function shakeImage() {
+  setTimeout(function () {
+    if(shaked){
+      document.getElementById("img-body-face").style.transform = "rotate(30deg)";
+      shaked = false;
+    } else {
+      document.getElementById("img-body-face").style.transform = "rotate(15deg)";
+      shaked = true;
+    }
+    shakeImage();
+  }, 1000);
+}
 
 fetch("https://pomber.github.io/covid19/timeseries.json")
   .then(response => response.json())
   .then((data) => {
 
-  setDate(getTodaysData(data).date);
-  setDaysLeft(getDaysLeft());
-  setConfirmed(getTodaysData(data).confirmed, getTodaysNewConfirmed(data));
-  setRecovered(getTodaysData(data).recovered, getTodaysNewRecovered(data));
-  setDeaths(getTodaysData(data).deaths, getTodaysNewDeaths(data));
-  setNewForTodayAndYesturdayData(data);
+    setDate(getTodaysData(data).date);
+    setDaysLeft(getDaysLeft());
+    setConfirmed(getTodaysData(data).confirmed, getTodaysNewConfirmed(data));
+    setRecovered(getTodaysData(data).recovered, getTodaysNewRecovered(data));
+    setDeaths(getTodaysData(data).deaths, getTodaysNewDeaths(data));
+    setNewForTodayAndYesturdayData(data);
 
-  let rateConfirmed = getTodaysRateConfirmed(data);
-  let rateRecovered = getTodaysRateRecovered(data);
-  let rateDeaths = getTodaysRateDeaths(data);
+    let rateConfirmed = getTodaysRateConfirmed(data);
+    let rateRecovered = getTodaysRateRecovered(data);
+    let rateDeaths = getTodaysRateDeaths(data);
 
-  setTodaysConfirmedRate(rateConfirmed);
-  setTodaysCoveredRate(rateRecovered);
-  setTodaysDeathsRate(rateDeaths);
+    setTodaysConfirmedRate(rateConfirmed);
+    setTodaysCoveredRate(rateRecovered);
+    setTodaysDeathsRate(rateDeaths);
 
-  setPicture(rateConfirmed, rateRecovered, rateDeaths);
+    setPicture(rateConfirmed, rateRecovered, rateDeaths);
 
-  placeMenita();
-    
-});
+    placeMenita();
+    shakeImage();
+
+  });
 
 
 
